49d2a9652d8e529bf3d49d9c300ac49c3db3d446
CCP Aporia <28982391+CCP-Aporia@users.noreply.github.com>
2026-05-22 13:21:36 +0000

    Stop using `crash-handler` as an inverted feature
    carbon-core has its own crash-handling mechanism, so Tracy's own crash-handler is not needed. As an inverted feature that's ON by default, however, the crash-handler would have to be explicitly disabled in every vcpkg component that uses carbon-core, due to how vcpkg resolves default features.

--- ports/tracy/portfile.cmake
+++ ports/tracy/portfile.cmake
@@ -17,6 +17,7 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         on-demand                        TRACY_ON_DEMAND
         fibers	                         TRACY_FIBERS
         verbose                          TRACY_VERBOSE
+        no-crash-handler                 TRACY_NO_CRASH_HANDLER  # Change from official port. Previous default crash-handler INVERTED_FEATURE becomes non-default regular FEATURE.
         manual-lifetime                  TRACY_MANUAL_LIFETIME
         delayed-init                     TRACY_DELAYED_INIT
         no-callstack                     TRACY_NO_CALLSTACK
@@ -38,9 +39,6 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         symbol-offline-resolve           TRACY_SYMBOL_OFFLINE_RESOLVE
         libbacktrace-elf-dynload-support TRACY_LIBBACKTRACE_ELF_DYNLOAD_SUPPORT
         ignore-memory-faults             TRACY_IGNORE_MEMORY_FAULTS
-
-    INVERTED_FEATURES
-        crash-handler TRACY_NO_CRASH_HANDLER
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS TOOLS_OPTIONS
